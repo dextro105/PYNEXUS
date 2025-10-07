2af23e63651d2dc28eaabb998965804bb4009c2a
dextro105 <115640580+dextro105@users.noreply.github.com>
2025-10-07 17:31:33 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import ScrollToTop from "./ScrollToTop"; // 👈 Add this import
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = () => {
   return (
-    <>
-      <ScrollToTop /> {/* 👈 This ensures scroll-to-top on route change */}
+    <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Navbar />
-      <main className="min-h-screen">
+      <main className="flex-grow">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
